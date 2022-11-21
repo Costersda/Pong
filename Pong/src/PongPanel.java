f07f7e30f49ad5286594742c0308c29eb7172c3e
Costersda <costersda@gmail.com>
2022-11-22 11:21:13 +1300

    added paint method

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,14 +4,31 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
  public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	 
 	 private final static Color BACKGROUND_COLOR = Color.BLACK;
 	 
+	 private final static int TIMER_DELAY = 5;
+	 
 	 public PongPanel() {
 		 setBackground(BACKGROUND_COLOR);
+		 
+		 Timer timer = new Timer(TIMER_DELAY, this);
+
+	         timer.start();
+	         
 	 }
+	 
+	 @Override
+	  public void paintComponent(Graphics g) {
+	      super.paintComponent(g);
+	      g.setColor(Color.WHITE);
+	      g.fillRect(20, 20, 100, 100);
+	  }
 
 	@Override
 	public void keyTyped(KeyEvent event) {
@@ -34,6 +51,10 @@ import javax.swing.JPanel;
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void update(){
 		
 	}
 
