a149feedd29da3ed6d945bd3b387f66582550534
Costersda <costersda@gmail.com>
2022-11-22 11:26:27 +1300

    added game line

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -7,6 +7,9 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+import java.awt.BasicStroke;
 
  public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	 
@@ -23,11 +26,19 @@ import java.awt.Graphics2D;
 	         
 	 }
 	 
+	 private void paintDottedLine(Graphics g) {
+		       Graphics2D g2d = (Graphics2D) g.create();
+		          Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+		          g2d.setStroke(dashed);
+		          g2d.setPaint(Color.WHITE);
+		          g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+		          g2d.dispose();
+		  }
+	 
 	 @Override
 	  public void paintComponent(Graphics g) {
 	      super.paintComponent(g);
-	      g.setColor(Color.WHITE);
-	      g.fillRect(20, 20, 100, 100);
+	      paintDottedLine(g);
 	  }
 
 	@Override
@@ -50,7 +61,8 @@ import java.awt.Graphics2D;
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		update();
+		repaint();
 		
 	}
 	
