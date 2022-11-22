a47cf14653aff94475e8627561707bce15d7a2e0
Costersda <costersda@gmail.com>
2022-11-23 12:52:07 +1300

    completed game

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -15,17 +16,30 @@ import java.awt.BasicStroke;
 	 
 	 private final static Color BACKGROUND_COLOR = Color.BLACK;	 
 	 private final static int TIMER_DELAY = 5;	  
+	 private final static int BALL_MOVEMENT_SPEED = 2;
+	 private final static int POINTS_TO_WIN = 3;
+	 private final static int SCORE_TEXT_X = 100;
+     private final static int SCORE_TEXT_Y = 100;
+     private final static int SCORE_FONT_SIZE = 50;
+     private final static String SCORE_FONT_FAMILY = "Serif";
+     private final static int WINNER_TEXT_X = 200;
+     private final static int WINNER_TEXT_Y = 200;
+     private final static int WINNER_FONT_SIZE = 40;
+     private final static String WINNER_FONT_FAMILY = "Serif";
+     private final static String WINNER_TEXT = "WIN!";
+	 int player1Score = 0, player2Score = 0;
+	 Player gameWinner;
 	 Ball ball;
 	 GameState gameState = GameState.INITIALISING;
 	 Paddle paddle1;
 	 Paddle paddle2;
 	 
 	 public PongPanel() {
-		 setBackground(BACKGROUND_COLOR);
-		 
+		 setBackground(BACKGROUND_COLOR);		 
 		 Timer timer = new Timer(TIMER_DELAY, this);
-
-	         timer.start();
+	     timer.start();
+	     addKeyListener(this);
+	     setFocusable(true);
 	         
 	 }
 	 
@@ -46,7 +60,8 @@ import java.awt.BasicStroke;
              paintSprite(g, ball);
              paintSprite(g, paddle1);
              paintSprite(g, paddle2);
- 
+             paintScores(g);
+             paintWinner(g);
          }
      }
 
@@ -58,21 +73,32 @@ import java.awt.BasicStroke;
 
 	@Override
 	public void keyPressed(KeyEvent event) {
-		// TODO Auto-generated method stub
-		
+		if(event.getKeyCode() == KeyEvent.VK_W) {
+			 paddle1.setyVelocity(-1);
+		 } else if(event.getKeyCode() == KeyEvent.VK_S) {
+			 paddle1.setyVelocity(1);
+		 }		
+		 if(event.getKeyCode() == KeyEvent.VK_UP) {
+			 paddle2.setyVelocity(-1);
+		 } else if(event.getKeyCode() == KeyEvent.VK_DOWN) {
+			 paddle2.setyVelocity(1);
+		 }
 	}
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-		// TODO Auto-generated method stub
-		
+		if(event.getKeyCode() == KeyEvent.VK_W || event.getKeyCode() == KeyEvent.VK_S) {
+			paddle1.setyVelocity(0);
+		}
+		if(event.getKeyCode() == KeyEvent.VK_UP || event.getKeyCode() == KeyEvent.VK_DOWN) {
+			paddle2.setyVelocity(0);
+		}		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		update();
-		repaint();
-		
+		repaint();		
 	}
 	
 
@@ -86,11 +112,19 @@ import java.awt.BasicStroke;
 	private void update() {
 		           switch(gameState) {
 		               case INITIALISING: {
-		                   createObjects();
+		                  createObjects();
 		                  gameState = GameState.PLAYING;
-		                   break;
+		                  ball.setxVelocity(BALL_MOVEMENT_SPEED);
+		                  ball.setyVelocity(BALL_MOVEMENT_SPEED);
+		                  break;
 		               }
 		               case PLAYING: {
+		            	   moveObject(paddle1);
+		                   moveObject(paddle2);
+		            	   moveObject(ball);            // Move ball
+		                   checkWallBounce();           // Check for wall bounce
+		                   checkPaddleBounce();         // Check for paddle bounce
+		                   checkWin();					// Check if the game has been won
 		                   break;
 		             }
 		             case GAMEOVER: {
@@ -103,7 +137,87 @@ import java.awt.BasicStroke;
 		     g.setColor(sprite.getColour());
 		     g.fillRect(sprite.getxPosition(), sprite.getyPosition(), sprite.getWidth(), sprite.getHeight());
 		}
+		
+		private void moveObject(Sprite obj) {
+			obj.setxPosition(obj.getxPosition() + obj.getxVelocity(),getWidth());
+		    obj.setyPosition(obj.getyPosition() + obj.getyVelocity(),getHeight());
+		}
+		
+		private void checkWallBounce(){
+			if(ball.getxPosition() <= 0) {
+		           // Hit left side of screen
+		           ball.setxVelocity(-ball.getxVelocity());
+		           addScore(Player.Two);
+				   resetBall();
+		       } else if(ball.getxPosition() >= getWidth() - ball.getWidth()) {
+		           // Hit right side of screen
+		           ball.setxVelocity(-ball.getxVelocity());
+		           addScore(Player.One);
+		    	   resetBall();
+		       }
+		       if(ball.getyPosition() <= 0 || ball.getyPosition() >= getHeight() - ball.getHeight()) {
+		           // Hit top or bottom of screen
+		           ball.setyVelocity(-ball.getyVelocity());
+			   }			
+		}
+		
+		private void resetBall() {
+			ball.resetToInitialPosition();
+		}
+		
+		private void checkPaddleBounce() {
+			      if(ball.getxVelocity() < 0 && ball.getRectangle().intersects(paddle1.getRectangle())) {
+			          ball.setxVelocity(BALL_MOVEMENT_SPEED);
+			      }
+			      if(ball.getxVelocity() > 0 && ball.getRectangle().intersects(paddle2.getRectangle())) {
+			          ball.setxVelocity(-BALL_MOVEMENT_SPEED);
+			      }
+		}
+		
+		private void addScore(Player player){
+			if (player == Player.One) {
+				player1Score++;
+			}
+			if (player == Player.Two){
+				player2Score++;
+			}
+			
+		}
+		
+		private void checkWin(){
+			if(player1Score >= POINTS_TO_WIN) {
+				gameWinner = Player.One;
+				gameState = GameState.GAMEOVER;
+			}
+			else if(player2Score >= POINTS_TO_WIN) {
+				gameWinner = Player.Two;
+				gameState = GameState.GAMEOVER;
+			}
+		}
+		
+		private void paintScores(Graphics g) {
+	          Font scoreFont = new Font(SCORE_FONT_FAMILY, Font.BOLD, SCORE_FONT_SIZE);
+	          String leftScore = Integer.toString(player1Score);
+	          String rightScore = Integer.toString(player2Score);
+	          g.setFont(scoreFont);
+	          g.drawString(leftScore, SCORE_TEXT_X, SCORE_TEXT_Y);
+	          g.drawString(rightScore, getWidth()-SCORE_TEXT_X, SCORE_TEXT_Y);
+	      }
+		
+		private void paintWinner(Graphics g) {
+	           if(gameWinner != null) {
+	               Font winnerFont = new Font(WINNER_FONT_FAMILY, Font.BOLD, WINNER_FONT_SIZE);
+	              g.setFont(winnerFont);
+	              int xPosition = getWidth() / 2;
+	              if(gameWinner == Player.One) {
+	                  xPosition -= WINNER_TEXT_X;
+	              } else if(gameWinner == Player.Two) {
+	                  xPosition += WINNER_TEXT_X;
+	              }
+	              g.drawString(WINNER_TEXT, xPosition, WINNER_TEXT_Y);
+	          }
+	      }
 	
-	
+		
 }
 
